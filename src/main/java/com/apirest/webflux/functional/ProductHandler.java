03e3995400e09f36fb6c3fadd270e203067930c4
Rafael Leite <rafanleite@gmail.com>
2021-02-11 09:50:05 -0300

    atualizacao da rota de getById e no README

--- src/main/java/com/apirest/webflux/functional/ProductHandler.java
+++ src/main/java/com/apirest/webflux/functional/ProductHandler.java
@@ -4,11 +4,8 @@ import com.apirest.webflux.data.Product;
 import com.apirest.webflux.service.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -20,6 +17,7 @@ public class ProductHandler {
 
     @Autowired
     ProductService productService;
+    Mono<ServerResponse> notFound = ServerResponse.notFound().build();
 
     public Mono<ServerResponse> save(ServerRequest request) {
         Mono<Product> product = request.bodyToMono(Product.class);
@@ -41,10 +39,10 @@ public class ProductHandler {
 
     public Mono<ServerResponse> getById(ServerRequest request) {
         String id = String.valueOf(request.pathVariable("id"));
-        Mono<ServerResponse> notFound = ServerResponse.notFound().build();
         Mono<Product> productMono = productService.getById(id);
 
-        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(productMono, Product.class)
+        return productMono.flatMap(
+                product -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(productMono, Product.class))
                 .switchIfEmpty(notFound);
 
     }
--- src/main/java/com/apirest/webflux/service/ProductService.java
+++ src/main/java/com/apirest/webflux/service/ProductService.java
@@ -4,11 +4,7 @@ import com.apirest.webflux.data.Product;
 import com.apirest.webflux.repository.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.reactive.function.server.ServerResponse;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
