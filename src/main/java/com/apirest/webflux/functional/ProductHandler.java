6a3a61bcf335ef8549df63b65e5aec7e89ec4b3b
Rafael Leite <rafanleite@gmail.com>
2021-02-12 13:39:12 -0300

    implementado handler para exception

--- src/main/java/com/apirest/webflux/functional/ProductHandler.java
+++ src/main/java/com/apirest/webflux/functional/ProductHandler.java
@@ -6,6 +6,7 @@ import com.apirest.webflux.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -36,12 +37,12 @@ public class ProductHandler {
         Mono<Void> deleted = productService.deleteById(id);
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(deleted, Void.class);
     }
-
+        
     public Mono<ServerResponse> getById(ServerRequest request) {
         String id = String.valueOf(request.pathVariable("id"));
         Mono<Product> productMono = productService.getById(id);
-
-        return productMono.flatMap(
+        
+        return  productMono.flatMap(
                 product -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(productMono, Product.class))
                 .switchIfEmpty(notFound);
 
